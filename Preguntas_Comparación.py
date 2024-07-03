8759cd601ee071ead5e8245cbcc5804e9db39605
Matienli <87873519+Matienli@users.noreply.github.com>
2024-07-03 14:29:29 -0300

    Add files via upload

---
+++ Preguntas_Comparación.py
@@ -0,0 +1,61 @@
+print("Pregunta #1")
+print(2==2)
+print("Pregunta #2")
+print(2==3)
+print("Pregunta #3")
+print(1==2)
+
+#print(var==0)
+print( )
+print ("Igualdad")
+var=0
+print(var==0)
+
+var_uno=1
+print(var_uno==0)
+
+print( )
+print("Desiguldad")
+
+var_dos= 0
+print(var_dos !=0)
+var_tres= 1
+print(var_tres!=0)
+
+print( )
+print("Mayor que")
+
+ovejas_negras=10
+ovejas_blancas=15
+print(ovejas_negras>ovejas_blancas)
+
+print( )
+print("Mayor o igual que")
+
+ovejas_blancas_1= 10
+ovejas_negras_1= 10
+print(ovejas_negras_1>ovejas_blancas_1)
+ovejas_blancas_2=10
+ovejas_negras_2=10
+print(ovejas_negras_2>=ovejas_blancas_2)
+
+print( )
+print("Menor o igual que")
+
+ovejas_negras_3= 10
+ovejas_blancas_3= 10
+print(ovejas_negras_3>ovejas_blancas_3)
+ovejas_negras_4= 10
+ovejas_blancas_4=10
+print(ovejas_negras_4<=ovejas_blancas_4)
+
+print()
+print("Uso de las respuestas")
+
+valorverdad=10>5
+valormentira=10>15
+print(valorverdad==valormentira)
+
+
+
+
